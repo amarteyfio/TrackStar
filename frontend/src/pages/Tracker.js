bb2fb55490833babb8288aa9a1ad871226d85a24
amarteyfio <amarteyfio>
2024-04-20 19:59:05 -0500

    added notes section to tracker details page

--- frontend/src/pages/Tracker.js
+++ frontend/src/pages/Tracker.js
@@ -16,13 +16,13 @@ const Tracker = () => {
         })
     }
     return (
-        <div className="counter-details">
+        <div className="tracker">
             {isPending && <div className="spinner"></div>}
             {error && <div></div>}
             {trk && (
                 <div className="tracker-details">
                     <h2>{trk.title}</h2>
-                    <p>Days: {}</p>
+                    <h3> 0 </h3>
                     <p>{trk.description}</p>
                     <p>Started On: {trk.start_date}</p>
                     <button onClick={() => {
@@ -32,7 +32,19 @@ const Tracker = () => {
                     }}>
                     Delete Tracker</button>
                 </div>
+                
             )}
+            <div className="notes">
+            <h2>Tracker Notes</h2>
+            <hr />
+            {trk ? (
+                trk.notes ? (
+                <p>{trk.notes}</p> // Render paragraph if notes exist
+                ) : (
+                <button>Add a Note</button> // Render button if notes don't exist
+                )
+            ) : null}
+            </div>
         </div>
     );
 }
